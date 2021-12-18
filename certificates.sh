a517ef9db9db1193c5c44525eae5324ddd477aae
drsherluck <danilobett@gmail.com>
2021-12-18 19:03:21 +0100

    rearrange project structure

--- certificates.sh
+++ certificates.sh
@@ -8,16 +8,16 @@ openssl req -x509 -new -nodes -sha256 -days 1024 -key ca.key -subj "/CN=localhos
 echo "Creating tls certificates"
 
 # server 
-server_dir=server
+server_dir=k8s/server
 openssl genrsa -out $server_dir/server.key 2048
 openssl req -new -key $server_dir/server.key -out $server_dir/server.csr -config $server_dir/csr.conf
 openssl x509 -req -in $server_dir/server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $server_dir/server.crt -days 365 -extfile $server_dir/csr.conf -extensions usr_ext
 
 # proxy
-proxy_dir=proxy
+proxy_dir=k8s/proxy
 openssl genrsa -out $proxy_dir/server.key 2048
 openssl req -new -key $proxy_dir/server.key -out $proxy_dir/server.csr -config $proxy_dir/csr.conf
 openssl x509 -req -in $proxy_dir/server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $proxy_dir/server.crt -days 365 -extfile $proxy_dir/csr.conf -extensions usr_ext
 
-ln -sf ../ca.crt $proxy_dir/ca.crt
+# ln -sf ../ca.crt $proxy_dir/ca.crt
 
--- run.sh
+++ run.sh
@@ -11,20 +11,22 @@ cp ca.crt $HOME/.minikube/certs/testca.crt
 # start and build
 minikube start --embed-certs
 eval $(minikube docker-env)
-docker build -t server server/.
-docker build -t proxy proxy/.
+docker build -t server src/server/.
+docker build -t proxy src/proxy/.
 
 # add tls secrets
+sdir=k8s/server
+pdir=k8s/proxy
 kubectl delete secret tls-secret-server tls-secret-proxy
-kubectl create secret tls tls-secret-server --cert=server/server.crt --key=server/server.key
-kubectl create secret tls tls-secret-proxy  --cert=proxy/server.crt --key=proxy/server.key
+kubectl create secret tls tls-secret-server --cert=$sdir/server.crt --key=$sdir/server.key
+kubectl create secret tls tls-secret-proxy  --cert=$pdir/server.crt --key=$pdir/server.key
 
 # create pods and serivces
-kubectl apply -k server
-kubectl apply -k proxy
+kubectl apply -k k8s/server
+kubectl apply -k k8s/proxy
 
 # start ingress
-kubectl apply -f ingress.yaml
+kubectl apply -f k8s/ingress.yaml
 
 # start service
 #minikube service proxy-service 
